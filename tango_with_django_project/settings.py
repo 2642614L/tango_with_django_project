dacfcc0b8c5951552629084fbc8234f1d3de4472
2642614L <2642614L@student.gla.ac.uk>
2022-02-21 08:38:39 +0800

    Chapter 9 exercises completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -97,6 +97,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = MEDIA_DIR
+
+LOGIN_URL = 'rango:login'
